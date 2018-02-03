76509fa6a6586684eda85aa2bbf9f49a380200ba
amalpha <amalpha123@gmail.com>
2018-02-03 17:47:02 +0900

    Add existing file

---
+++ Searching/binary_search - Copy.py
@@ -0,0 +1,26 @@
+
+# coding: utf-8
+
+
+def iterative_bin_search(lists,target):
+    
+    max_index = len(lists)-1
+    min_index = 0
+    found = False
+    
+    while min_index <= max_index and not found:
+        mid_index = (max_index + min_index) // 2
+        if target == lists[mid_index]:
+            print(target,' is found in the list!')
+            found = True
+            break
+        else:
+                if target > lists[mid_index]:
+                    min_index = mid_index+1
+                else:
+                    max_index = mid_index-1
+                    
+    if found == False:
+        print(target,' is not found in the list!')
+
+def recursive_bin_search(lists,target):
